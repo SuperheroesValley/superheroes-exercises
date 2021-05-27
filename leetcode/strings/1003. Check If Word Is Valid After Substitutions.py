9bf09377029adc785b4439d2f4c1fd83ba8f6ca2
patacca <patacca@autistici.org>
2021-05-27 20:23:48 +0200

    Some more comments

--- leetcode/strings/1003. Check If Word Is Valid After Substitutions.py
+++ leetcode/strings/1003. Check If Word Is Valid After Substitutions.py
@@ -1,18 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # La cima dello stack indica quale deve essere il prossimo carattere
         stack = []
         
+        # Scorriamo la sequenza e se il carattere corrente e' uguale alla cima dello stack allora lo rimuoviamo dallo stack
+        # e aggiungiamo in cima allo stack il successivo elemento ('a' -> 'b', 'b' ->, 'c' -> niente)
+        # NOTA: in qualsiasi momento possiamo trovare una 'a', infatti in qualsiasi punto possiamo
+        # inserire un nuovo 'abc'
         for c in s:
             if c == 'a':
-                stack.append('b')
+                stack.append('b') # Il prossimo elemento nella sequenza che ci aspettiamo e' una 'b'
             elif stack and c == stack[-1]:
                 stack.pop()
                 if c == 'b':
-                    stack.append('c')
+                    stack.append('c') # Il prossimo elemento nella sequenza che ci aspettiamo e' una 'b'
             else:
-                return False
+                return False # c'e' un mismatch, la sequenza non va bene
         
-        return not stack
+        return not stack # se lo stack e' vuoto ritorna True
 
 
 # Another solution, same complexity
@@ -21,11 +26,12 @@ class Solution:
         ans = "abc"
         i = 0
         
+        # Generiamo progressivamente la soluzione scorrendo di una posizione alla volta la sequenza
         while len(ans) < len(s):
             if i < len(ans) and ans[i] == s[i]:
-                i += 1
+                i += 1 # la sequenza matcha la nostra sequenza generata finora
             else:
-                if s[i] == 'a':
+                if s[i] == 'a': # Se abbiamo un mismatch e nella sequenza c'e' una 'a', allora inseriamo un 'abc' nel punto indicato
                     ans = ans[:i] + "abc" + ans[i:]
                 else:
                     return False
